70af7ccbe716e306cefc05fe257ed58f00abf2ed
saiumesh535 <d.sai535@gmail.com>
2020-04-24 14:04:37 +0530

    added transactions to migration

--- src/postgres_db.rs
+++ src/postgres_db.rs
@@ -5,6 +5,11 @@ use crate::errors::*;
 
 pub struct PGClient(pub Client);
 
+pub struct QueryTransaction {
+    pub query: String,
+    pub message: String
+}
+
 impl PGClient {
     // check if migration table available
     pub fn check_migration_table(&mut self) -> Result<()> {
@@ -18,15 +23,32 @@ impl PGClient {
         Ok(self.0.query("SELECT file_name FROM migrations", &[]).context(DBErrorConfig { })?)
     }
 
-    pub fn run_query(&mut self, query: &str, message: String) -> Result<()> {
-        self.0.query(query, &[]).context(MigrationFileConfig {
-            message
-        })?;
-        Ok(())
-    }
+    // pub fn run_query(&mut self, query: &str, message: String) -> Result<()> {
+    //     self.0.query(query, &[]).context(MigrationFileConfig {
+    //         message
+    //     })?;
+    //     Ok(())
+    // }
 
     pub fn insert_migration_file(&mut self, file_name: &str) -> Result<()> {
         self.0.query("INSERT INTO migrations (file_name) VALUES ($1)", &[&file_name], ).context(DBErrorConfig { })?;
         Ok(())
     }
+
+    pub fn transaction(&mut self, query_transactions: Vec<QueryTransaction>) -> Result<()> {
+        let mut transaction = self.0.transaction().context(
+            DBErrorConfig { }
+        )?;
+
+        for query_transactions in query_transactions {
+            transaction.execute(query_transactions.query.as_str(), &[]).context(MigrationFileConfig {
+                message: query_transactions.message
+            })?;
+        }
+        transaction.commit().context(
+            DBErrorConfig { }
+        )?;
+        Ok(())
+    }
+
 }
